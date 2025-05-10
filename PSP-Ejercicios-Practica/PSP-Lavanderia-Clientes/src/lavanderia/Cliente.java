4807ef5115e48076d9205aed26b9a8f5f9acd59e
acensito <acenforos@gmail.com>
2025-05-10 13:50:12 +0200

    lavanderia

--- PSP-Ejercicios-Practica/PSP-Lavanderia-Clientes/src/lavanderia/Cliente.java
+++ PSP-Ejercicios-Practica/PSP-Lavanderia-Clientes/src/lavanderia/Cliente.java
@@ -5,6 +5,22 @@ package lavanderia;
  *
  * @author DGGC
  */
-public class Cliente {
+public class Cliente extends Thread {
+    private final Lavanderia lavanderia;
+
+    public Cliente(Lavanderia lavanderia, String name) {
+        super(name);
+        this.lavanderia = lavanderia;
+    }
+
+    @Override
+    public void run() {
+        if (lavanderia.isEstado()) {
+            //si esta abierta la lavanderia, intenta usar una lavadora
+            lavanderia.usarLavadora(this.getName());
+        } else {
+            System.out.println("Llegó y estaba cerrada la lavandería");
+        }
+    }
     
 }
--- PSP-Ejercicios-Practica/PSP-Lavanderia-Clientes/src/lavanderia/Lavanderia.java
+++ PSP-Ejercicios-Practica/PSP-Lavanderia-Clientes/src/lavanderia/Lavanderia.java
@@ -1,46 +1,68 @@
 
 package lavanderia;
 
-import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Clase Lavanderia
+ * 
  * @author Felipe R.
  */
 public class Lavanderia {
-    private static final boolean estado = true; //estado apertura lavanderia
-    private static int clientes_atendidos = 0;
-    private static int recaudacion = 0;
-    private static final ArrayList<Lavadora> lavadoras = new ArrayList<>();
+    private boolean estado = true;       //estado apertura lavanderia
+    private int clientes = 0;            //num clientes atendidos
+    private int funcionando = 0;         //contador lavadoras funcionando
+    private final int num_lavadoras;                  //num lavadoras de la lavanderia
+    private final Object lock = new Object();   //objeto lock apoyo
+    private final Random random = new Random(); //objeto random
 
-    /**
-     * Constructor de la clase. Recibe el numero de lavadoras que va a tener la
-     * lavandería
-     * 
-     * @param num_lavadoras 
-     */
     public Lavanderia(int num_lavadoras) {
-        //instanciamos el numero de lavadoras a funcionar
-        for (int i = 0; i < num_lavadoras; i++) {
-            lavadoras.add(new Lavadora("Lavadora-"+i));
-        }
+        this.num_lavadoras = num_lavadoras;
     }
-    
-    /**
-     * Devuelve la primera lavadora que se encuentre libre del array de lavadoras
-     * @return objeto lavadora que queda libre
-     */
-    private Lavadora buscaLavadoraLibre() {
-        //recorremos las lavadoras en busca de la primera libre en el array
-        for (Lavadora lavadora : lavadoras) {
-            if (!lavadora.isLavando()) {
-                //se devuelve dicha lavadora
-                return lavadora;
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado() {
+        this.estado = false;
+    }
+
+    public void usarLavadora(String cliente) {
+        if (estado) { //mientras se encuentra abierto el local 
+            try {
+                synchronized (lock) {
+                    while (funcionando >= num_lavadoras) {
+                        //mientras no haya lavadores disponibles
+                        System.out.println(cliente + " esperando a una lavadora disponible...");
+                        lock.wait();
+                    }
+
+                    funcionando++; //contamos una lavadora funcionando
+                    clientes++; //sumamos un cliente atendido
+                    System.out.println(cliente + " empieza a lavar. Lavadoras funcionando: " + funcionando);
+                }
+                
+                //simulamos un periodo de lavado entre 5-10 segundos
+                Thread.sleep(random.nextInt(5000,10001));
+                    
+                synchronized (lock) {
+                    funcionando--; //libera una lavadora
+                    System.out.println(cliente + "finaliza. Lavadoras funcionando: " + funcionando);
+                    lock.notifyAll(); //notificamos al resto de hilos
+                }
+            } catch (InterruptedException e) {
+                System.err.println("ERROR");
             }
         }
-        return null;
+
     }
-    
-    
+
+    public void getEstado(){
+        System.out.println("Clientes atendidos: " + clientes);
+        System.out.println("Ganancia total: " + clientes*3 + "€");
+        System.out.println("Lavadoras disponibles: " + (num_lavadoras-funcionando));
+        System.out.println("Lavadoras en uso: " + funcionando);
+    } 
     
 }
